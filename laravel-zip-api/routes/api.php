036f44fbf9b6d4bc7aaf1192369127a737a41c21
gczybarna <gczy.barna@gmail.com>
2025-10-13 01:49:29 +0200

    add postman collection for api

---
+++ laravel-zip-api/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CountyController;
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\PostalCodeController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/counties', [CountyController::class, 'index']);
+Route::get('/counties/{county}', [CountyController::class, 'show']);
+
+Route::get('/cities', [CityController::class, 'index']);
+Route::get('/cities/{city}', [CityController::class, 'show']);
+
+Route::get('/postal-codes', [PostalCodeController::class, 'index']);
+Route::get('/postal-codes/{postalCode}', [PostalCodeController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::post('/counties', [CountyController::class, 'store']);
+    Route::put('/counties/{county}', [CountyController::class, 'update']);
+    Route::delete('/counties/{county}', [CountyController::class, 'destroy']);
+    
+    Route::post('/cities', [CityController::class, 'store']);
+    Route::put('/cities/{city}', [CityController::class, 'update']);
+    Route::delete('/cities/{city}', [CityController::class, 'destroy']);
+    
+    Route::post('/postal-codes', [PostalCodeController::class, 'store']);
+    Route::put('/postal-codes/{postalCode}', [PostalCodeController::class, 'update']);
+    Route::delete('/postal-codes/{postalCode}', [PostalCodeController::class, 'destroy']);
+});
